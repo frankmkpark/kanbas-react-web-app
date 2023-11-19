69e254e3b8a35c3ae0efe40dcfc619c4d3a81a98
frankmkpark <frankmkpark@gmail.com>
2023-11-19 05:45:02 -0500

    Upto 4.2.4

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -3,34 +3,55 @@ import KanbasNavigation from "./KanbasNavigation";
 import Courses from "./Courses";
 import Account from "./Account";
 import Dashboard from "./Dashboard";
-import db from "./Database";
-import { useState } from "react";
+// import db from "./Database";
+import { useState, useEffect } from "react";
 import store from "./store";
 import { Provider } from "react-redux";
+import axios from "axios";
 
 function Kanbas() {
-  const [courses, setCourses] = useState(db.courses);
+  const [courses, setCourses] = useState([]);
+  const URL = "http://localhost:4000/api/courses";
+  const findAllCourses = async () => {
+    const response = await axios.get(URL);
+    setCourses(response.data);
+  };
+  useEffect(() => {
+    findAllCourses();
+  }, []);
+
   const [course, setCourse] = useState({
     name: "New Course",      number: "New Number",
     startDate: "2023-09-10", endDate: "2023-12-15",
   });
-  const addNewCourse = () => {
-    setCourses([...courses, { ...course, _id: new Date().getTime().toString() }]);
+  const addNewCourse = async () => {
+    try {
+      const response = await axios.post(URL, course);
+      setCourses([response.data, ...courses]);
+      setCourse({ name: "", number: "", startDate: "", endDate: "" });
+    } catch (error) {
+      console.error("Error adding new course:", error);
+    }
   };
-  const deleteCourse = (courseId) => {
-    setCourses(courses.filter((course) => course._id !== courseId));
+
+  const deleteCourse = async (courseId) => {
+    try {
+      await axios.delete(`${URL}/${courseId}`);
+        setCourses(courses.filter((course) => course._id !== courseId));
+    } catch (error) {
+      console.error("Error deleting course:", error);
+    }
   };
-  const updateCourse = () => {
-    setCourses(
-      courses.map((c) => {
-        if (c._id === course._id) {
-          return course;
-        } else {
-          return c;
-        }
-      })
-    );
+
+  const updateCourse = async (courseToUpdate) => {
+    try {
+      const response = await axios.put(`${URL}/${courseToUpdate._id}`, courseToUpdate);  
+      setCourses(courses.map((c) => c._id === response.data._id ? response.data : c));
+    } catch (error) {
+      console.error("Error updating course:", error);
+    }
   };
+
   return (
     <Provider store={store}>
     <div className="d-flex">
